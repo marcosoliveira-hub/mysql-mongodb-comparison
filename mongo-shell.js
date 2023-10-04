ae1f871f46b82eab0c5a1bd05c074f1db064205c
Marcos Vinicius de Oliveira <76614140+marcosoliveira-hub@users.noreply.github.com>
2023-10-04 15:20:50 -0300

    Update mongo-shell.js

--- mongo-shell.js
+++ mongo-shell.js
@@ -1,1 +1,111 @@
+db.estudantes.insertMany([
+  {
+    "nome": "João Silva",
+		"idade": 20,
+    "sexo": "Masculino",
+    "curso": "Engenharia Elétrica",
+    "materias": [
+      {
+        "nome": "Cálculo I",
+        "carga_horaria": 60,
+        "media": 8.5
+      },
+      {
+        "nome": "Física I",
+        "carga_horaria": 45,
+        "media": 7.2
+      }
+    ]
+  },
+  {
+    "nome": "Maria Santos",
+    "idade": 22,
+    "sexo": "Feminino",
+    "curso": "Ciência da Computação",
+    "materias": [
+      {
+        "nome": "Programação em Python",
+        "carga_horaria": 50,
+        "media": 9.0
+      },
+      {
+        "nome": "Banco de Dados",
+        "carga_horaria": 55,
+        "media": 8.2
+      }
+    ]
+  },
+  {
+    "nome": "Pedro Oliveira",
+    "idade": 21,
+    "sexo": "Masculino",
+    "curso": "Medicina",
+    "materias": [
+      {
+        "nome": "Anatomia Humana",
+        "carga_horaria": 70,
+        "media": 9.5
+      },
+      {
+        "nome": "Histologia",
+        "carga_horaria": 55,
+        "media": 8.7
+      }
+    ]
+  },
+  {
+    "nome": "Ana Souza",
+    "idade": 19,
+    "sexo": "Feminino",
+    "curso": "Administração",
+    "materias": [
+      {
+        "nome": "Gestão de Projetos",
+        "carga_horaria": 45,
+        "media": 7.8
+      },
+      {
+        "nome": "Economia",
+        "carga_horaria": 60,
+        "media": 7.0
+      }
+    ]
+  },
+  {
+    "nome": "Lucas Ferreira",
+    "idade": 23,
+    "sexo": "Masculino",
+    "curso": "Psicologia",
+    "materias": [
+      {
+        "nome": "Psicologia Clínica",
+        "carga_horaria": 65,
+        "media": 8.9
+      },
+      {
+        "nome": "Psicopatologia",
+        "carga_horaria": 55,
+        "media": 8.4
+      }
+    ]
+  },
+  {
+    "nome": "Mariana Oliveira",
+    "idade": 21,
+    "sexo": "Feminino",
+    "curso": "Engenharia Elétrica",
+    "materias": [
+      {
+        "nome": "Cálculo I",
+        "carga_horaria": 60,
+        "media": 5.2
+      },
+      {
+        "nome": "Sistemas de Potência",
+        "carga_horaria": 60,
+        "media": 8.2
+      }
+    ]
+  }
+]);
 

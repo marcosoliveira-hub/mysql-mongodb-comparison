cecf4cc39bec76be351596480566fbf10cc47a58
Marcos Vinicius de Oliveira <76614140+marcosoliveira-hub@users.noreply.github.com>
2023-10-05 21:01:04 -0300

    Update mongo-shell.js

--- mongo-shell.js
+++ mongo-shell.js
@@ -1,3 +1,6 @@
+// Criação da coleção estudantes e a inserção dos dados
+// (Lembre-se que a coleção é criada automaticamente caso ela não exista)
+
 db.estudantes.insertMany([
   {
     "nome": "João Silva",
@@ -109,3 +112,65 @@ db.estudantes.insertMany([
   }
 ]);
 
+// Dados da coleção
+
+db.estudantes.find()
+
+// Seleção com filtragem
+
+db.estudantes.find({idade: {$lte: 20}})
+
+// Alteração (update) da idade de um estudante
+
+db.estudantes.updateOne(
+  { nome: 'Maria Santos' },
+  { $set: { idade: 23 } }
+);
+
+// Deleção de todos os estudantes com 21 anos ou menos
+
+db.estudantes.deleteMany({ idade: { $lte: 21 } });
+
+// Criação de índice para o campo nome (o número 1 é para indicar que a ordenação do índice deve ser feita em ordem crescente)
+
+db.estudantes.createIndex({ nome: 1 });
+
+// Exemplo de transação (alterando idade, inserindo uma matéria a mais para o aluno João Silva e aplicando as mudanças com commit)
+
+const session = db.getMongo().startSession();
+session.startTransaction();
+
+try {
+  db.estudantes.updateOne(
+    { nome: 'João Silva' },
+    { $set: { idade: 21 } },
+    { session }
+  );
+
+  db.notas.insertOne(
+    {
+      aluno: 'João Silva',
+      materia: 'Cálculo II',
+      nota: 9.3
+    },
+    { session }
+  );
+
+  session.commitTransaction();
+} catch (error) {
+  print(`Error occurred: ${error}`);
+  session.abortTransaction();
+} finally {
+  session.endSession();
+}
+
+// Exemplo de agregação mostrando a média de idade de todos os alunos, separando-os pelo sexo
+
+db.estudantes.aggregate([
+  {
+    $group: {
+      _id: '$sexo',
+      avg_age: { $avg: '$idade' }
+    }
+  }
+]);
